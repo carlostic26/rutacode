b895596d96680d67a32c9544ff99feff3f2db809
junior-iba1904 <127237500+junior-iba1904@users.noreply.github.com>
2025-05-26 11:00:30 -0500

    Junior CPP section completed

---
+++ lib/common/feature/content/data/datasources/languages/C++/insert_mid_c++.dart
@@ -0,0 +1,13 @@
+import 'package:sqflite_common/sqlite_api.dart';
+
+Future<void> insertMidLevel1Data(Database db) async {
+
+}
+
+Future<void> insertMidLevel2Data(Database db) async {
+  
+}
+
+Future<void> insertMidLevel3Data(Database db) async {
+  
+}
