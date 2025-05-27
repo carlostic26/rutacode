090cf795e27d4397abc8690c40578fa54069f5f8
carlostic26 <carlospenaranda26@gmail.com>
2025-05-27 08:56:16 -0500

    changing to cpp name

---
+++ lib/common/feature/content/data/datasources/languages/cpp/main_insert_cpp.dart
@@ -0,0 +1,32 @@
+import 'package:rutacode/common/feature/content/data/datasources/languages/Cpp/insert_jr_cpp.dart';
+import 'package:rutacode/common/feature/content/data/datasources/languages/Cpp/insert_mid_cpp.dart';
+import 'package:rutacode/common/feature/content/data/datasources/languages/Cpp/insert_sr_cpp.dart';
+import 'package:sqflite/sqflite.dart';
+
+class InsertCppData {
+  static Future<void> insertCppData(Database db) async {
+    await _insertCppJrLevelData(db);
+    await _insertCppMidLevelData(db);
+    await _insertCppSrLevelData(db);
+  }
+
+  static Future<void> _insertCppJrLevelData(Database db) async {
+    await insertJrLevel1DataCpp(db);
+    await insertJrLevel2DataCpp(db);
+    await insertJrLevel3DataCpp(db);
+  }
+
+  static Future<void> _insertCppMidLevelData(Database db) async {
+    await insertMidLevel1DataCpp(db);
+    await insertMidLevel2DataCpp(db);
+    await insertMidLevel3DataCpp(db);
+  }
+
+  static Future<void> _insertCppSrLevelData(Database db) async {
+    await insertSrLevel1DataCpp(db);
+    await insertSrLevel2DataCpp(db);
+    await insertSrLevel3DataCpp(db);
+    await insertSrLevel4DataCpp(db);
+    await insertSrLevel5DataCpp(db);
+  }
+}
